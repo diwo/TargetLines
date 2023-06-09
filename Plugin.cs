5d137b635cfe92b5f5e1af47c600656a87a1e4aa
Drahsid <Drahsid@gmail.com>
2023-06-09 04:11:24 -0400

    bump version

--- Plugin.cs
+++ Plugin.cs
@@ -12,7 +12,7 @@ using Dalamud.Game.ClientState.Conditions;
 using System.IO;
 using TargetLines.Attributes;
 
-[assembly: System.Reflection.AssemblyVersion("1.2.3")]
+[assembly: System.Reflection.AssemblyVersion("1.2.4")]
 
 namespace TargetLines;
 
