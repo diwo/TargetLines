7d42e7d020bac36ce0b130343042c02261dc471e
Drahsid <Drahsid@gmail.com>
2024-07-02 23:05:38 -0400

    Bump version

--- TargetLines/AssemblyAttributes.cs
+++ TargetLines/AssemblyAttributes.cs
@@ -4,7 +4,7 @@
 [assembly: System.Reflection.AssemblyProduct("TargetLines")]
 [assembly: System.Reflection.AssemblyTitle("TargetLines")]
 
-[assembly: System.Reflection.AssemblyVersion("1.5.1")]
+[assembly: System.Reflection.AssemblyVersion("1.6.0")]
 
 #if DEBUG
 [assembly: System.Reflection.AssemblyConfiguration("Debug")]
